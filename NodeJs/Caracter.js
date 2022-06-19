6c02d18cbf124eb8d40985672f859ce8f9897414
Matty <74145047+Matteo0099@users.noreply.github.com>
2022-06-19 13:35:54 +0200

    Update Caracter.js
    ritocchi alla versione js

--- NodeJs/Caracter.js
+++ NodeJs/Caracter.js
@@ -13,15 +13,8 @@ typewriter
 .typeString("It...")
 .pauseFor(500)
 .deleteChars(5)
-function highlight(text) {
-    var inputText = document.getElementById("inputText");
-    var innerHTML = inputText.innerHTML;
-    var index = innerHTML.indexOf(text);
-    if (index >= 0) { 
-     innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-     inputText.innerHTML = innerHTML;
-    }  
-}
+.typeString("Italy!")
+.start();
 
 //Controllo
 // Si può usare anche usare direttamente un carattere non BMP
