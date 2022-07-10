99381aad7fd6fa728ac5a925a9479b54ba3e2816
Matty <74145047+Matteo0099@users.noreply.github.com>
2022-07-11 00:25:38 +0200

    Rimodernamento totale sito

--- src/onclick.js
+++
@@ -1,5 +0,0 @@
-$('button#navButton1.btn').on('click', function (event) {
-    $('.arrow', event.target)
-        .toggleClass('rotate')
-        .toggleClass('reset');
-});
--- src/onclickFunction.js/onclick.js
+++
@@ -1,3 +0,0 @@
-$('#Calque_1').click(function(){
-    $('#Calque_1').css('transform', 'scale(1,-1) ');// here point at same id, but could be any other selecteur if you wish
-});
