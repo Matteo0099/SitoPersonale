ee6b43b8ea4f38f288f28b5e014440c77b399388
Matty <matteomania05@gmail.com>
2022-08-11 00:11:15 +0200

    Restyling (aggiunta)

---
+++ src/toggle.js
@@ -0,0 +1,11 @@
+//Get the element
+let topBtn = document.querySelector(".top-btn");
+
+//On Click, Scroll to the page's top, replace 'smooth' with 'auto' if you don't want smooth scrolling
+topBtn.onclick = () => window.scrollTo({
+    top: 0, behavior: "smooth" 
+});
+
+//On scroll, Show/Hide the btn (with animation)
+window.onscroll = () => window.scrollY > 650 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0;
+//Final//
