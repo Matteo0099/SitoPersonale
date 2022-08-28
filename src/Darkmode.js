19124368fbc3a9a1e3987a1488455dbe0a987a62
Matty <matteomania05@gmail.com>
2022-08-28 16:12:59 +0200

    Some aggiunte

---
+++ src/Darkmode.js
@@ -0,0 +1,8 @@
+$('#myonoffswitch').change(function () {
+    $('html, body, #titolo, .primary-navbar, #colonna, .box, #ion-dark, .button').toggleClass('dark-mode');
+});
+
+/*function Dark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}*/
