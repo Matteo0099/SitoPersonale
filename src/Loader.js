b9c0a3978aaab1b83ab09040a1b3713b76f3082b
Matty <matteomania05@gmail.com>
2022-08-28 21:08:52 +0200

    Some aggiustament

---
+++ src/Loader.js
@@ -0,0 +1,19 @@
+/*window.onload = function () {
+   alert("LOADED!");
+}*/
+
+/*function myFunction() {
+   alert("Page is loaded");
+}*/
+
+/*var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 10);
+}
+
+function showPage() {
+  document.querySelector("loading").style.display = "none";
+  document.querySelector("my-content").style.display = "block";
+}
+*/
--- src/closeToggle.js
+++ src/closeToggle.js
@@ -32,30 +32,6 @@ function toggleItem(elem) {
   toggleItem(document.querySelectorAll('.navbar-burger'));
 */
 
-//canvas experiment
-let canvas = document.querySelector('#main');
-if(canvas.getContext) {
-   let ctx = main.getContext('2d');
-}
-
-(() => {
-    const canvas = document.querySelector('section');
-    if (!canvas.getContext) {
-        return;
-    }
-
-    // get the context
-    let ctx = canvas.getContext('2d');
-
-    // set fill and stroke styles
-    ctx.fillStyle = '#0B3866';
-    ctx.strokeStyle = '#0B3866';
-
-    // draw a rectangle with fill and stroke
-    ctx.fillRect(50, 50, 150, 100);
-    ctx.strokeRect(50, 50, 150, 100);
-})();
-
 /*function Closenav() {
     $('navbar-menu').click(function (e) {
         if ($(this).hasClass("active")) {
