484e7dd59e665c567d4675064789d1b1d4a08a3d
Matty <matteomania05@gmail.com>
2022-09-08 12:08:24 +0200

    Some Fixes

--- src/Darkmode.js
+++ src/Darkmode.js
@@ -1,5 +1,5 @@
 $('#myonoffswitch').change(function () {
-    $('html, body, #titolo, .primary-navbar, #colonna, .box, #ion-dark, .button').toggleClass('dark-mode');
+    $('html, body, #titolo, .primary-navbar, #colonna, .box, #ion-dark, .button, #arrow, #myBtn').toggleClass('dark-mode');
 });
 
 /*function Dark() {
---
+++ src/backToTop.js
@@ -0,0 +1,19 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 655 || document.documentElement.scrollTop > 655) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox, IE and Opera
+} 
