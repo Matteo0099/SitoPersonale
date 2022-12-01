72c7d90585a0d5d06d3878acd88bbf31d0c86c11
Matteo0099 <matteomania05@gmail.com>
2022-12-01 17:29:51 +0100

    Some fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Contact from '../components/contact';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
 import {
   AiFillInstagram,
@@ -186,9 +185,33 @@ export default function Home() {
           </div>
         </section>
 
-        <Contact>
-          
-        </Contact>
+        <section className='min-h-max flex flex-col pt-4 pb-6'>
+          <div className='pt-2 mt-2 mx-auto flex flex-col justify-center max-w-5xl'>
+            <h1 className='text-black dark:text-gray-200 text-4xl py-1 mt-5 pt-5 pb-4 text-center mx-auto font-bold'>Contact</h1>
+            <ul className='flex flex-row p-4 text-gray-600 dark:text-gray-400 border-t-2'>
+              <li className='mx-4 text-4xl'>
+                <a href="https://www.instagram.com/itz_matteomania/" className='hover:text-gray-700 dark:hover:text-gray-100'>
+                  <AiFillInstagram />
+                </a>
+              </li>
+              <li className='mx-4 text-4xl'>
+                <a href="https://www.facebook.com/robin.ticke.50/" className='hover:text-gray-700 dark:hover:text-gray-100'>
+                  <AiFillFacebook />
+                </a>
+              </li>
+              <li className='mx-4 text-4xl'>
+                <a href="https://github.com/Matteo0099" className='hover:text-gray-700 dark:hover:text-gray-100'>
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className='mx-4 text-4xl'>
+                <a href="mailto:matteomania09@gmail.com" className='hover:text-gray-700 dark:hover:text-gray-100'>
+                  <AiFillMessage />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
       </main>
     </div>
   );
