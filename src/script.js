839b241fb347a27ccf5697941103f9a9fdf5fd3e
Matty <74145047+Matteo0099@users.noreply.github.com>
2022-12-24 00:42:40 +0100

    Summary

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 //open nav
 //toggle 
 function openNav() {
-	console.log("open nav")
-	document.getElementById("nav-page").classList.toggle("visible")
+    console.log("open nav")
+    document.getElementById("nav-page").classList.toggle("visible")
     console.log("close nav")
-	document.getElementById("nav").classList.toggle("invisible")
-	document.getElementById("body").classList.toggle("no-overflow")
+    document.getElementById("nav").classList.toggle("invisible")
+    document.getElementById("body").classList.toggle("no-overflow")
 }
 
 /**
@@ -39,37 +39,111 @@ function animate() {
 }
 
 /**
- * toggle Dark-mode
+ * Parallax Image scroll
 */
+<<<<<<< Updated upstream
 /*
 window.onload = function () {
     setMode();
+=======
+let image_parallax = document.querySelectorAll('.image-parallax');
+let hero_parallax = document.querySelectorAll('.about .container');
+let contactTitle_parallax = document.querySelectorAll('.contact__title');
+
+//scroll on page
+window.addEventListener('scroll', function () {
+    let win_pos = window.pageYOffset;
+    image_parallax.forEach((item) => {
+        let elementToMove = item.getBoundingClientRect();
+        //enter in the window
+        if (win_pos > elementToMove.top) {
+            item.style.transform = "translateY(" + elementToMove.top * -.1 * 0.525 + "px)";
+        }
+    });
+    hero_parallax.forEach((item) => {
+        let elementToMove = item.getBoundingClientRect();
+        if (win_pos > elementToMove.top) {
+            item.style.transform = "translateY(" + elementToMove.top * .1 * 0.625 + "px)";
+        }
+    });
+    contactTitle_parallax.forEach((item) => {
+        let elementToMove = item.getBoundingClientRect();
+        if (win_pos > elementToMove.top) {
+            item.style.transform = "translateY(" + elementToMove.top * .1 * 0.875 + "px)";
+        }
+    });
+});
+
+/**
+ * preference user theme browser
+*/
+// The default settings
+const swiperSettings = {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: ".swiper-pagination"
+    }
+>>>>>>> Stashed changes
 };
 
-function setMode() {
-    let mode = localStorage.getItem('mode');
-    if (!mode) mode = 'light';
-    document.body.classList.add(mode);
+// Grab a reference to the media query.
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+// if the user asked for no anmiations:
+if (!mediaQuery || mediaQuery.matches) {
+    swiperSettings.effect = "fade";
+    swiperSettings.speed = 0;
+    swiperSettings.autoplay = false;
+} else {
+    swiperSettings.effect = "slide";
+    swiperSettings.speed = 300;
+    swiperSettings.autoplay = { delay: 2500 };
 }
 
-function toggleMode() {
-    let element = document.body;
-    let btn = document.getElementById("darkmode-button");
-    let mode = localStorage.getItem('mode')
+// Initialize the slider with the correct settings
+let swiper = new Swiper(".swiper-container", swiperSettings);
 
-    if (!mode) mode = 'dark';
+// Listen for changes in the media query.
+mediaQuery.addEventListener("change", () => {
+    // Unset the slider instance.
+    swiper.destroy();
 
-    if (mode == 'dark') {
-        localStorage.setItem("mode", "light");
-        element.classList.remove("dark");
-        element.classList.add("light");
-        // btn.innerHTML = 'Dark mode';
+    // if the user asked for no anmiations.
+    if (mediaQuery.matches) {
+        swiperSettings.effect = "fade";
+        swiperSettings.speed = 0;
+        swiperSettings.autoplay = false;
+    } else {
+        swiperSettings.effect = "slide";
+        swiperSettings.speed = 300;
+        swiperSettings.autoplay = { delay: 2500 };
     }
-    else {
-        localStorage.setItem("mode", "dark");
-        element.classList.remove("light");
-        element.classList.add("dark");
-        // btn.innerHTML = 'Light mode';
-    }
+
+    // re-initialize the slider with the correct settings.
+    swiper = new Swiper(".swiper-container", swiperSettings);
+});
+
+// double check 
+// Check if the media query matches or is not available.
+if (!mediaQuery || mediaQuery.matches) {
+    doSomethingWithoutAnimation();
+} else {
+    doSomethingWithAnimation();
 }
+<<<<<<< Updated upstream
 */
+=======
+
+// Ads an event listener to check for changes in the media query's value.
+mediaQuery.addEventListener("change", () => {
+    if (mediaQuery.matches) {
+        doSomethingWithoutAnimation();
+    } else {
+        doSomethingWithAnimation();
+    }
+});
+>>>>>>> Stashed changes
