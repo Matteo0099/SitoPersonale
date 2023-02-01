f52b196b78c321fcbec3e005e247e750c531075b
Matteo0099 <matteomania05@gmail.com>
2023-02-01 18:47:36 +0100

    Some refresh

--- src/script.js
+++ src/script.js
@@ -3,43 +3,22 @@ let cursor = document.querySelector('.cursor');
 let links = document.querySelectorAll('a');
 let btn = document.querySelectorAll('button');
 
+//update the cursor
 document.addEventListener('mousemove', function (e) {
     let x = e.clientX;
     let y = e.clientY;
-
     outline.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`; //alt96
     cursor.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
-
-    //ciclo sui link per l'effetto hover
-    links.forEach((link) => {
-        link.addEventListener("mouseover", function () {
-            outline.classList.add('hover-crs');
-            cursor.classList.add('hover-crs');
-        });
-        link.addEventListener("mouseleave", function () {
-            outline.classList.remove('hover-crs');
-            cursor.classList.remove('hover-crs');
-        });
-        if (e.target !== this) {
-            e.stopPropagation();
-            localStorage.clear();
-        }
-    });
-    btn.forEach((button) => {
-        button.addEventListener("mouseover", function () {
-            outline.classList.add('hover-crs');
-            cursor.classList.add('hover-crs');
-        });
-        button.addEventListener("mouseleave", function () {
-            outline.classList.remove('hover-crs');
-            cursor.classList.remove('hover-crs');
-        });
-        if (e.target !== this) {
-            e.stopPropagation();
-            localStorage.clear();
-        }
-    });
 });
+
+function mouseCursor(event) {
+    outline.classList.toggle('hover-crs');
+    cursor.classList.toggle('hover-crs');
+    if (event.target !== this) {
+        event.stopPropagation();
+        localStorage.clear();
+    }
+}
 
 //open nav
 //toggle 
@@ -49,36 +28,6 @@ function openNav() {
     console.log("close nav")
     document.getElementById("nav").classList.toggle("invisible")
     document.getElementById("body").classList.toggle("no-overflow")
-}
-
-/**
- * preloader 
- * 4 cicle loader animation 
-**/
-var loader = document.querySelector('.preloader');
-var loader__container = document.querySelector('.holder');
-var loadingscreen = document.querySelector('.full__page');
-var TotalLoading = setInterval(animate, 100);  //interval
-//about 3 seconds  3+3 < 99
-var count = 1;   //initial
-var costantGrowth = 0.1;
-const max = 4; //max count
-//if 1->4 with 0.1 (1 sec = 0.1*10) = 4(4sec)
-
-console.log("loading...");
-
-function animate() {
-    count += costantGrowth;
-    console.log(count);
-    //stop count
-    if (count == max || count > max) {
-        loader.style.display = "none";
-        loadingscreen.style.display = "none";
-        loader__container.style.display = "none";
-        stop(count);
-        clearInterval(TotalLoading);
-        console.log("loaded!");
-    }
 }
 
 /**
@@ -104,3 +53,33 @@ window.addEventListener('scroll', function () {
         }
     });
 });
+
+//delete web cache
+window.onbeforeunload = function () {
+    document.stopPropagation();
+    localStorage.clear();
+    Cache.delete();
+    CacheStorage.delete();
+}
+
+/*document.addEventListener('mousemove', function (e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    outline.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`; //alt96
+    cursor.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
+    //ciclo sui link per l'effetto hover
+    links.forEach((link) => {
+        link.addEventListener("mouseover", function () {
+            outline.classList.add('hover-crs');
+            cursor.classList.add('hover-crs');
+        });
+        link.addEventListener("mouseleave", function () {
+            outline.classList.remove('hover-crs');
+            cursor.classList.remove('hover-crs');
+        });
+        if (link.target !== this) {
+            e.stopPropagation();
+            localStorage.clear();
+        }
+    });
+});*/
