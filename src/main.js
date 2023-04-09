65b864413ac839d489cbcaad6dc9ae5074c1bc11
Matteo0099 <matteomania05@gmail.com>
2023-04-09 17:39:50 +0200

    Refresh

--- src/main.js
+++ src/main.js
@@ -1,20 +1,95 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './assets/tailwind.css'
-// Import Swiper and modules
-import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+class StickyNavigation {
 
-// Swiper.js
-const swiper = new Swiper('.swiper', {
-  // Install modules
-  modules: [Navigation, Pagination, Scrollbar],
-  speed: 500,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    constructor() {
+        this.currentId = null;
+        this.currentTab = null;
+        this.tabContainerHeight = 70;
+        let self = this;
+        $('.et-hero-tab').click(function () {
+            self.onTabClick(event, $(this));
+        });
+        $(window).scroll(() => { this.onScroll(); });
+        $(window).resize(() => { this.onResize(); });
+    }
 
-const app = createApp(App);
-app.mount('#app');
+    onTabClick(event, element) {
+        event.preventDefault();
+        let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+        $('html, body').animate({ scrollTop: scrollTop }, 600);
+    }
+
+    onScroll() {
+        this.checkTabContainerPosition();
+        this.findCurrentTabSelector();
+    }
+
+    onResize() {
+        if (this.currentId) {
+            this.setSliderCss();
+        }
+    }
+
+    checkTabContainerPosition() {
+        let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+        if ($(window).scrollTop() > offset) {
+            $('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+        }
+        else {
+            $('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+        }
+    }
+
+    findCurrentTabSelector(element) {
+        let newCurrentId;
+        let newCurrentTab;
+        let self = this;
+        $('.et-hero-tab').each(function () {
+            let id = $(this).attr('href');
+            let offsetTop = $(id).offset().top - self.tabContainerHeight;
+            let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+            if ($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+                newCurrentId = id;
+                newCurrentTab = $(this);
+            }
+        });
+        if (this.currentId != newCurrentId || this.currentId === null) {
+            this.currentId = newCurrentId;
+            this.currentTab = newCurrentTab;
+            this.setSliderCss();
+        }
+    }
+
+    setSliderCss() {
+        let width = 0;
+        let left = 0;
+        if (this.currentTab) {
+            width = this.currentTab.css('width');
+            left = this.currentTab.offset().left;
+        }
+        $('.et-hero-tab-slider').css('width', width);
+        $('.et-hero-tab-slider').css('left', left);
+    }
+
+}
+
+new StickyNavigation();
+
+
+$(document).ready(function () {
+    // typing animation
+    (function ($) {
+      $.fn.writeText = function (content) {
+        var contentArray = content.split(""),
+          current = 0,
+          elem = this;
+        setInterval(function () {
+          if (current < contentArray.length) {
+            elem.text(elem.text() + contentArray[current++]);
+          }
+        }, 80);
+      };
+    })(jQuery);
+  
+    // input text for typing animation
+    $("#holder").writeText("Web development, design.");
+})
