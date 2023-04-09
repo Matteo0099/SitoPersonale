9e40526aee2f3917cb44d91e8768f1c1c4dd91a2
Matteo0099 <matteomania05@gmail.com>
2023-04-09 18:02:48 +0200

    Fix 2

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import './style.css'
 import './sass/base/reset.css'
 import './sass/main.css'
+//script
+import './src/main.js'
+import './src/Particles.js'
+import './src/JqueryV3.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
