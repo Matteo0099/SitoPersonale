cc3e1e14bb138880ed4e2353f46dfecd0160159e
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-04-12 22:29:43 +0200

    Added threejs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 class StickyNavigation {
-
     constructor() {
         this.currentId = null;
         this.currentTab = null;
@@ -78,18 +77,18 @@ new StickyNavigation();
 $(document).ready(function () {
     // typing animation
     (function ($) {
-      $.fn.writeText = function (content) {
-        var contentArray = content.split(""),
-          current = 0,
-          elem = this;
-        setInterval(function () {
-          if (current < contentArray.length) {
-            elem.text(elem.text() + contentArray[current++]);
-          }
-        }, 80);
-      };
+        $.fn.writeText = function (content) {
+            var contentArray = content.split(""),
+                current = 0,
+                elem = this;
+            setInterval(function () {
+                if (current < contentArray.length) {
+                    elem.text(elem.text() + contentArray[current++]);
+                }
+            }, 80);
+        };
     })(jQuery);
-  
+
     // input text for typing animation
     $("#holder").writeText("Web development, design.");
 })
