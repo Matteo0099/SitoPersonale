c1c4f147a2864108eb596c1134b64b4e3efc9210
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-06-17 20:48:40 +0200

    some update

--- src/script.js
+++ src/script.js
@@ -1,24 +1,23 @@
-let toggleDark = document.querySelector('.toggle-dark')
-let toggleLight = document.querySelector('.bi-moon')
-let toggleOscuro = document.querySelector('.bi-moon-fill')
-let body = document.body;
+const toggleDark = document.querySelector('.toggle-dark');
+const toggleLight = document.querySelector('.bi-moon');
+const toggleOscuro = document.querySelector('.bi-moon-fill');
+const body = document.body;
 
-const theme = localStorage.getItem('theme');
-if (theme) {
-  body.classList.add(theme);
-  if (theme === 'dark') {
-    toggleLight.style.display = 'flex';
-    toggleOscuro.style.display = 'none';
-  } else {
-    toggleLight.style.display = 'none';
-    toggleOscuro.style.display = 'flex';
-  }
+function updateTheme(theme) {
+  body.className = theme;
+  toggleLight.style.display = theme === 'dark' ? 'flex' : 'none';
+  toggleOscuro.style.display = theme === 'dark' ? 'none' : 'flex';
+}
+
+const storedTheme = localStorage.getItem('theme');
+
+if (storedTheme) {
+  updateTheme(storedTheme);
 }
 
 toggleDark.addEventListener('click', () => {
-  body.classList.toggle('dark');
   const isDark = body.classList.contains('dark');
-  localStorage.setItem('theme', isDark ? 'dark' : 'light');
-  toggleOscuro.style.display = isDark ? 'none' : 'flex';
-  toggleLight.style.display = isDark ? 'flex' : 'none';
+  const newTheme = isDark ? 'light' : 'dark';
+  updateTheme(newTheme);
+  localStorage.setItem('theme', newTheme);
 });
