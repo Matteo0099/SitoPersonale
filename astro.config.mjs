60b632f81ce7cdc6513c39f6a773741b642e760c
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-08-10 12:03:33 +0200

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-cip.netlify.app',
+  site: 'https://matteomania.netlify.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap()]
