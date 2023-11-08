2c697058eb456c75ae745a02d6c908f270d912ae
Matteo Manià <matteomania05@gmail.com>
2023-11-08 18:42:06 +0100

    some updates

---
+++ src/js/TextSplit.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let interval = null;
+
+  document.querySelectorAll("h1").forEach(h1Element => {
+    h1Element.addEventListener('mouseover', event => {
+      let iteration = 0;
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration)
+              return event.target.dataset.value[index];
+
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+
+        if (iteration >= event.target.dataset.value.length)
+          clearInterval(interval);
+
+        iteration += 0.4;
+      }, 30);
+    });
+  });
+});
